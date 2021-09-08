ae192ab5b58dbc9028b157fdd46b9ad0859a41b1
vincent-guo <vincent.weiguo@gmail.com>
2021-09-08 23:16:51 +0800

    Initial Commit

---
+++ Shared/DesignCodeCourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeCourseApp.swift
+//  Shared
+//
+//  Created by Vincent Guo on 2021/9/8.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeCourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
