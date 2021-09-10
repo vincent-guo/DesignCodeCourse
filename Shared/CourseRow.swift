43850a4b82f727042fdff092f480afc8006fdfc3
vincent-guo <vincent.weiguo@gmail.com>
2021-09-10 11:31:46 +0800

    3 SFSymbolsListStyles

---
+++ Shared/CourseRow.swift
@@ -0,0 +1,40 @@
+//
+//  CourseRow.swift
+//  DesignCodeCourse
+//
+//  Created by Vincent Guo on 2021/9/10.
+//
+
+import SwiftUI
+
+struct CourseRow: View {
+    var body: some View {
+        HStack(alignment: .top) {
+            Image(systemName: "paperplane.circle.fill")
+                .renderingMode(.template)
+                .frame(width: 48.0, height: 48.0)
+                .imageScale(.medium)
+                .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.blue/*@END_MENU_TOKEN@*/)
+                .clipShape(Circle())
+                .foregroundColor(.white)
+            
+            VStack(alignment: .leading, spacing: 4.0) {
+                Text("SwiftUI")
+                    .font(.subheadline)
+                    .bold()
+                
+                Text("Description")
+                    .font(.footnote)
+                    .foregroundColor(.secondary)
+            }
+            
+            Spacer()
+        }
+    }
+}
+
+struct CourseRow_Previews: PreviewProvider {
+    static var previews: some View {
+        CourseRow()
+    }
+}
---
+++ Shared/CoursesView.swift
@@ -0,0 +1,23 @@
+//
+//  CoursesView.swift
+//  DesignCodeCourse
+//
+//  Created by Vincent Guo on 2021/9/10.
+//
+
+import SwiftUI
+
+struct CoursesView: View {
+    var body: some View {
+        List(0 ..< 20) { item in
+            CourseRow()
+        }
+        .listStyle(InsetGroupedListStyle())
+    }
+}
+
+struct CoursesView_Previews: PreviewProvider {
+    static var previews: some View {
+        CoursesView()
+    }
+}
